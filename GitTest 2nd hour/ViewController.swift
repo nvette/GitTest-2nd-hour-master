0450c412a0cc33812cee722985364f47b50bfc9e
NATHAN VETTE RUER <nathan.vetteruer21@clcmbp23.local>
2019-04-29 09:47:03 -0500

    delete 3rd

--- GitTest 2nd hour/ViewController.swift
+++ GitTest 2nd hour/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 // hello
     // goodbye
-    // 3rd tryt
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
