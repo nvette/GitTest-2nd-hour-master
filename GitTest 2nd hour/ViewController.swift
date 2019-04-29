db54e025b24ba384fbbe17fa85b5db71a85e387b
NATHAN VETTE RUER <nathan.vetteruer21@clcmbp23.local>
2019-04-29 09:27:03 -0500

    Initial Commit

---
+++ GitTest 2nd hour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest 2nd hour
+//
+//  Created by Brian Seaver on 4/29/19.
+//  Copyright © 2019 clc.seaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
